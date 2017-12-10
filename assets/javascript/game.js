eac44d85869f726caf9134054ae051b53d71b3a3
mjsimon91 <mjsimon91@gmail.com>
2017-12-10 13:18:19 -0500

    HTML is done and now adding my psuedo code for javascript

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+// Create variables for the chrystals, wins, score to beat, losses, and your scoreToBeat
+
+//Create a function for the start of the Game
+
+//Create a function for each additional Game
+
+//Create a function for applying a random value to the score to beat which will be between 19 and 120
+
+//Create a function for applying a random value for Chrystal_1 between 1 and 12
+//Create a function for applying a random value for Chrystal_2 between 1 and 12
+//Create a function for applying a random value for Chrystal_3 between 1 and 12
+//Create a function for applying a random value for Chrystal_4 between 1 and 12
+
+//Create a function for adding a value to the score
